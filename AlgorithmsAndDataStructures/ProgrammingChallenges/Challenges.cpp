c0304c15fa2ea551c13913d4bd703b5a2841b403
nicholas_rawitscher <nicholas.rawitscher@fojab.se>
2020-02-15 00:14:48 +0100

    Initial commit

---
+++ AlgorithmsAndDataStructures/ProgrammingChallenges/Challenges.cpp
@@ -0,0 +1,8 @@
+#include "Challenges.h"
+#include<vector>
+//using namespace Challenges
+
+int Challenges::SumOfTwoDigits(std::vector<int>& numbers)
+{
+
+}
---
+++ AlgorithmsAndDataStructures/ProgrammingChallenges/Challenges.h
@@ -0,0 +1,18 @@
+
+
+#include <iostream>
+#include <vector>
+
+//namespace Challenges
+//{
+	class Challenges
+	{
+	public:
+
+		int SumOfTwoDigits(std::vector<int>& numbers);
+
+
+
+	};
+//}
+
